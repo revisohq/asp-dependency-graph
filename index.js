4495d2f0489b072701d4470e10a615c66701d237
Benjamin Horsleben <benjamin@fizkerinc.dk>
2015-09-02 17:06:53 +0200

    A folder can be globbed and analyzed

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+import glob from 'glob'
+import path from 'path'
+import throat from 'throat'
 import analyzer from './src/analyze-asp-file'
 
-var file = process.argv[2]
-analyzer(file).then(console.log, console.error)
+var baseDir = process.argv[2]
+
+var p = new Promise((resolve, reject) => {
+	glob('**/*.asp', { cwd: baseDir }, (err, files) => err ? reject(err) : resolve(files))
+})
+
+p.then(files => files.map(throat(1, file => analyzer(path.join(baseDir, file)))))
+	.then(a => Promise.all(a))
+	.then(console.log, e => {console.error(e.stack);process.exit(1)})
