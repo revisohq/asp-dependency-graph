679b6d97880f5d0c4f093db44fc6f2edb5f269e3
Benjamin Horsleben <benjamin@fizkerinc.dk>
2015-09-02 16:43:38 +0200

    Basic data is extracted from asp files

--- src/analyze-asp-file.js
+++ src/analyze-asp-file.js
@@ -1,10 +1,19 @@
 import fs from 'fs'
+import path from 'path'
 import split from 'split'
 import eos from 'end-of-stream'
 
+const funcRegex = /^function +([A-Z_0-9]+)/i
+const subRegex = /^sub +([A-Z_0-9]+)/i
+const includeRegex = /^<!-- +#include +file *= *"([^"]+)"/i
+const aspClientRegex = /^ASPClient *\. *([A-Z_0-9]+)/i
+
 export default function(file) {
+	var dirname = path.dirname(file)
 	var data = {
 		file,
+		classes: [],
+		aspClientCalls: [],
 		includes: [],
 		subs: [],
 		funcs: [],
@@ -13,15 +22,32 @@ export default function(file) {
 	var currentLine = ''
 	var inputStream = fs.createReadStream(file)
 		.pipe(split())
-		.on('data', line => {
-			if(line.endsWith('_')) {
-				currentLine += line.substring(0, line.length - 1)
+		.on('data', l => {
+			if(l.endsWith('_')) {
+				currentLine += l.substring(0, l.length - 1)
 				return
 			}
-			currentLine += line
-			console.log('line:', currentLine)
-
+			currentLine = (currentLine + l).trim()
+			var line = currentLine
 			currentLine = ''
+
+			var match
+			if(match = line.match(funcRegex)) {
+				data.funcs.push(match[1])
+				return
+			}
+			if(match = line.match(subRegex)) {
+				data.subs.push(match[1])
+				return
+			}
+			if(match = line.match(includeRegex)) {
+				data.includes.push(path.join(dirname, match[1]))
+				return
+			}
+			if(match = line.match(aspClientRegex)) {
+				data.aspClientCalls.push(match[1])
+				return
+			}
 		})
 
 	return new Promise((resolve, reject) => {
